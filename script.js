ee1d6f0aced5df0a30daff041cae016f79c58e9e
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-11-16 16:21:38 -0600

    adds working modal window for review

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Replace me with your JavaScript!
+window.addEventListener("load", function() {
+	var addNoteButton = document.getElementById("add-note");
+	addNoteButton.addEventListener("click", displayModal);
+
+	function displayModal() {
+		var modalElements = document.getElementsByClassName("modal");
+		console.log(modalElements.length);
+		for (var i = 0; i < modalElements.length; i++) {
+			console.log("Starting on index " + i + " of modalElements");
+			modalElements[i].classList.toggle("modal-hide");
+		}
+	}
+});
